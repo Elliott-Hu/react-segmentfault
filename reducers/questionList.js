a7bea1e098217e551eea12aca9035c1cfe520ccc
handsomeBee <278669145@qq.com>
2016-11-14 18:28:22 +0800

    combineReducers

--- reducers/questionList.js
+++ reducers/questionList.js
@@ -3,32 +3,31 @@ import { createStore,combineReducers } from "redux";
 
 var initialState = {
 	question_type:0,
-	question_list:[],
+	question_list:[{
+		"title":"tcp_tw_reuse 和 tcp_tw_recycle 在原理上有什么区别？",
+		"rank_vote":0,
+		"rank_answer":0,
+		"rank_view":21,
+		"new_desc":"3小时前提问",
+		"author":"cholerae",
+		"tags":["服务器","linux","tcp","网络"],
+		"status":0,
+	},{
+		"title":"为啥从文件流中读取数据到新链表，要求返回头指针head，需要定义多一个临时指针p ?",
+		"rank_vote":0,
+		"rank_answer":0,
+		"rank_view":32,
+		"new_desc":"3小时前提问",
+		"author":"伊利醇牛奶",
+		"tags":["c"],
+		"status":0,
+	}],
 }
 
 function question_list(state = initialState.question_list,action){
 	switch (action.type){
 		case "RENDER_QUESTION_LIST":
-			var	question_list = [{
-					"title":"tcp_tw_reuse 和 tcp_tw_recycle 在原理上有什么区别？",
-					"rank_vote":0,
-					"rank_answer":0,
-					"rank_view":21,
-					"new_desc":"3小时前提问",
-					"author":"cholerae",
-					"tags":["服务器","linux","tcp","网络"],
-					"status":0,
-				},{
-					"title":"为啥从文件流中读取数据到新链表，要求返回头指针head，需要定义多一个临时指针p ?",
-					"rank_vote":0,
-					"rank_answer":0,
-					"rank_view":32,
-					"new_desc":"3小时前提问",
-					"author":"伊利醇牛奶",
-					"tags":["c"],
-					"status":0,
-				}]
-			return question_list;
+			return action.question_list;
 			break;
 		default:
 			return state;
